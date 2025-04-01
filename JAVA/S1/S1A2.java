2fe36421201d874f7decaee746e06d12f9f04e1a
shinchan5683 <kaushalsheth12@gmail.com>
2025-04-01 12:04:58 +0530

    Update S1A2.java

--- JAVA/S1/S1A2.java
+++ JAVA/S1/S1A2.java
@@ -2,66 +2,53 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.sql.*;
-class A1 extends JFrame implements ActionListener{
-  JPanel jp1,jp2;
-  JLabel jl1,jl2,jl3,jl4;
-  JTextField jtf1,jtf2,jtf3,jtf4;
-  JButton jb1;
-  Connection con;
-  Statement stmt;
-  String url="jdbc:postgresql://localhost:5432/product";
-  String usr="postgres";
-  String pwd="root";
-  A1(){
-    try{
-      con=DriverManager.getConnection(url,usr,pwd);
-      stmt=con.createStatement();
-      setTitle("Employee Details");
-      setSize(400,400);
-      setLayout(new BorderLayout());
-      jp1 = new JPanel();
-      jp1.setLayout(new GridLayout(4,2));
-      jl1 = new JLabel("Employee No");
-      jtf1 = new JTextField(20);
-      jl2 = new JLabel("Employee Name");
-      jtf2 = new JTextField(20);
-      jl3 = new JLabel("Designation");
-      jtf3 = new JTextField(20);
-      jl4 = new JLabel("Salary");
-      jtf4 = new JTextField(20);
-      jb1 = new JButton("Submit");
-      jp1.add(jl1);
-      jp1.add(jtf1);
-      jp1.add(jl2);
-      jp1.add(jtf2);
-      jp1.add(jl3);
-      jp1.add(jtf3);
-      jp1.add(jl4);
-      jp1.add(jtf4);
-      add(jp1,BorderLayout.CENTER);
-      add(jb1, BorderLayout.SOUTH); 
-      setVisible(true);
-      jb1.addActionListener(this);
-      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      }catch(Exception e){
-      System.out.println(e);
-      }
-    }
-      public void actionPerformed(ActionEvent ae){
-        try{
-          if(ae.getSource()==jb1){
-            String eno=jtf1.getText();
-            String ename=jtf2.getText();
-            String desig=jtf3.getText();
-            String sal=jtf4.getText();
-            stmt.executeUpdate("Insert into Employee values('"+eno+"','"+ename+"','"+desig+"','"+sal+"')");
-            System.out.println("Record Inserted");
-          }
-        }catch(Exception e){
-          System.out.println(e);
+
+class EmployeeForm extends JFrame implements ActionListener {
+    private JTextField jtf1, jtf2, jtf3, jtf4;
+    private JButton jb1;
+    private Connection con;
+    private Statement stmt;
+
+    EmployeeForm() {
+        try {
+            con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/product", "postgres", "root");
+            stmt = con.createStatement();
+        } catch (Exception e) {
+            System.out.println(e);
         }
-      }
-      public static void main(String[] args){
-        new A1();
-      }
+
+        setTitle("Employee Details");
+        setSize(400, 300);
+        setLayout(new BorderLayout());
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel jp1 = new JPanel(new GridLayout(4, 2));
+        String[] labels = {"Employee No", "Employee Name", "Designation", "Salary"};
+        JTextField[] fields = {jtf1 = new JTextField(), jtf2 = new JTextField(), jtf3 = new JTextField(), jtf4 = new JTextField()};
+        
+        for (int i = 0; i < labels.length; i++) {
+            jp1.add(new JLabel(labels[i]));
+            jp1.add(fields[i]);
+        }
+
+        jb1 = new JButton("Submit");
+        jb1.addActionListener(this);
+        
+        add(jp1, BorderLayout.CENTER);
+        add(jb1, BorderLayout.SOUTH);
+        setVisible(true);
     }
+
+    public void actionPerformed(ActionEvent ae) {
+        try {
+            stmt.executeUpdate("INSERT INTO Employee VALUES ('" + jtf1.getText() + "', '" + jtf2.getText() + "', '" + jtf3.getText() + "', '" + jtf4.getText() + "')");
+            System.out.println("Record Inserted");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        new EmployeeForm();
+    }
+}
